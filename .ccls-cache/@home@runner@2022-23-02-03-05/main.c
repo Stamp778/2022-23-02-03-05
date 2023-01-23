f49719b7312ae4c028ecd07be81ee303c502d919
Gabe Olsen <73495349+Stamp778@users.noreply.github.com>
2023-01-23 15:16:53 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@2022-23-02-03-05/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+  char s1[] = "Hello";
+  char s2[] = "";
+  
+  size_t len_s1 = strlen(s1);
+  printf("%s", s1);
+
+  while (len_s1 != -1) {
+    len_1--1;
+    printf(len_1);
+  }
+  return 0;
+}
